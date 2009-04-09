27c9dfe473e2fb5c46bd9126c0699af685114e52
Luca Fabbri <luca@keul.it>
2009-04-09 14:41:56 +0000

    1.1.2: fix finali alla portlet

--- cciaa/intranetworkflow/skins/cciaa_workflow/my_camcom_worklist.py
+++ cciaa/intranetworkflow/skins/cciaa_workflow/my_camcom_worklist.py
@@ -9,28 +9,36 @@
 ##
 
 if context.portal_membership.isAnonymousUser():
-    return []
+    return ([], False)
 
 wf_results = list(context.portal_workflow.getWorklistsResults())
+if not wf_results:
+    return ([], False)
+
 wf_results.sort(lambda x, y: cmp(x.modified(), y.modified()))
 member = context.portal_membership.getAuthenticatedMember()
+portal_workflow = context.portal_workflow
+
+hasReviewLev2 = False
 
 # Ulteriore filtro, se sono un caposervizio vedo quelli del capoufficio SOLO se ho un parametro particolare
 # Altrimenti ricontrollo tutti gli elementi ed elimino quelli non nello stato giusto
+tmpList = []
+for j in wf_results:
+    if portal_workflow.getInfoFor(j, 'review_state', '???')=='attesa':
+        if (
+            member.has_permission('CamCom: Approvazione livello 1',j) and \
+            not member.has_permission('CamCom: Approvazione livello 2',j)
+            ) or \
+            (
+             member.has_permission('CamCom: Approvazione livello 2',j) and \
+             context.REQUEST.get('showcuff')=='y'
+            ):
+            tmpList.append(j)
+    elif portal_workflow.getInfoFor(j, 'review_state', '???')=='attesa_cser' and not context.REQUEST.get('showcuff',None):
+        if member.has_permission('CamCom: Approvazione livello 2',j):
+            hasReviewLev2 = True
+            tmpList.append(j)
 
-if 'CSer' in member.getRolesInContext(context) and (not context.REQUEST.get('showcuff') or context.REQUEST.get('showcuff')!='y'):
-    tmpList = []
-    portal_workflow = context.portal_workflow
-    for j in wf_results:
-        if portal_workflow.getInfoFor(j, 'review_state', '???')=='attesa_cser':
-            tmpList.append(j)
-    wf_results = tmpList
-elif 'CSer' in member.getRolesInContext(context):
-    # Sono CSer ma voglio solo i documenti del CUff
-    tmpList = []
-    portal_workflow = context.portal_workflow
-    for j in wf_results:
-        if portal_workflow.getInfoFor(j, 'review_state', '???')=='attesa':
-            tmpList.append(j)
-    wf_results = tmpList
-return wf_results
+wf_results = tmpList
+return (wf_results, hasReviewLev2)
